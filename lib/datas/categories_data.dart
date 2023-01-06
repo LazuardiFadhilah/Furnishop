7d537266c4c2b1cbd1460a6a4cfa3da18e656e26
Lazuardi Fadhilah <lazuardifadhilah@192.168.1.8>
2023-01-06 22:54:20 +0700

    add models and dummy data

---
+++ lib/datas/categories_data.dart
@@ -0,0 +1,11 @@
+import '../models/categories.dart';
+
+List<Categories> dummyData = [
+  Categories(id: 'c1', title: 'chair', image: 'assets/chair_icon.png'),
+  Categories(id: 'c2', title: 'tabble', image: 'assets/tabble_icon.png'),
+  Categories(id: 'c3', title: 'cupboard', image: 'assets/cupboard_icon.png'),
+  Categories(id: 'c4', title: 'lamp', image: 'assets/lamp_icon.png'),
+  Categories(id: 'c5', title: 'bed', image: 'assets/bed_icon.png'),
+  Categories(id: 'c6', title: 'sofa', image: 'assets/sofa_icon.png'),
+  Categories(id: 'c7', title: 'cook', image: 'assets/cook_icon.png'),
+];
---
+++ lib/models/categories.dart
@@ -0,0 +1,11 @@
+class Categories {
+  final String id;
+  final String title;
+  final String image;
+
+  Categories({
+    required this.id,
+    required this.title,
+    required this.image,
+  });
+}
