4b1769a6eac5aa98a6ce9897314cf66fb17d555c
Lazuardi Fadhilah <lazuardifadhillah@gmail.com>
2023-01-06 14:02:52 +0700

    second upload test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './styles.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,7 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: whiteColor,
+      ),
     );
   }
 }
